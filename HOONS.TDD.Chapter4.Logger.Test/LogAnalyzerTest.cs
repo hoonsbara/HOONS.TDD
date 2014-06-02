00692574217f9222427b91010f8ad21c27cd6f32
JoelPark <jpark@1up.fm>
2014-06-02 11:11:28 +0100

    chapter4

---
+++ HOONS.TDD.Chapter4.Logger.Test/LogAnalyzerTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using HOONS.TDD.Chapter2.TaxCalculator;
+using NUnit.Framework;
+using Moq;
+
+namespace HOONS.TDD.Chapter4.Logger.Test
+{
+    public class MockService : IWebService
+    {
+        public string LastError
+        {
+            get { return _lastError; }
+        }
+
+        private string _lastError;
+
+        public void LogError(string message)
+        {
+            _lastError = message;
+        }
+    }
+
+
+    [TestFixture]
+    public class LogAnalyzerTest
+    {
+
+        [Test]
+        public void WhenAnalyze_ShouldReturnTooShortFileName()
+        {
+            var mockService = new MockService();
+            var log = new LogAnalyzer(mockService);
+            string tooShortFileName = "abc.ext";
+
+            log.Analyze(tooShortFileName);
+
+            Assert.That(mockService.LastError, Is.EqualTo("Filename too short:abc.ext"));
+        }
+
+        [Test]
+        public void When2013_ShouldReturn90Percent_UsingMoq()
+        {
+            //Arrange
+            var taxRepo = new Mock<ITaxRepository>();
+            taxRepo.Setup(x => x.GetTaxRate(It.IsAny<TaxYear>())).Returns(10);
+            var taxHelper = new TaxHelper(TaxYear.Year2013, taxRepo.Object);
+            const int salaryExpected = 900;
+
+            //Act
+            var salaryResulted = taxHelper.Calculate(1000);
+
+            //Assert
+            Assert.That(salaryResulted, Is.EqualTo(salaryExpected));
+        }
+
+
+        [Test]
+        public void WhenAnalyze_ShouldReturnTooShortFileName_WithMock()
+        {
+            var mockService = new Mock<IWebService>(MockBehavior.Strict);
+
+            string tooShortFileName = "abc.ext";
+            mockService.Setup(x => x.LogError("Filename too short:abc.ext")).Verifiable();
+
+            var log = new LogAnalyzer(mockService.Object);
+
+            log.Analyze(tooShortFileName);
+
+            //Assert.That(strLastError, Is.EqualTo("Filename too short:abc.ext"));
+            mockService.Verify(x => x.LogError("Filename too short:abc.ext"));
+            //mockService.Verify(x => x.LogError(It.IsAny<string>()));
+        }
+
+        [Test]
+        //[ExpectedException]
+        public void WhenAnalyze_ShouldReturnTooShortFileName_WithCallback()
+        {
+            //Arrange
+            var mockService = new Mock<IWebService>(MockBehavior.Strict);
+            string strLastError = "";
+            string tooShortFileName = "abc.ext";
+
+            mockService.Setup(x => x.LogError(It.IsAny<string>(
+                )))
+                .Callback(
+                    (string error) =>
+                    {
+                        strLastError = error;
+                    }
+                );
+            //mockService.Setup(x => x.LogError("Tsest")).Throws(new ArgumentException());
+            var log = new LogAnalyzer(mockService.Object);
+
+            //Act
+            log.Analyze(tooShortFileName);
+
+            //Assert
+            Assert.That(strLastError, Is.EqualTo("Filename too short:abc.ext"));
+        }
+    }
+}
--- HOONS.TDDChapter2.TexCalculator.Test/TaxCalculatorTest.cs
+++ HOONS.TDDChapter2.TexCalculator.Test/TaxCalculatorTest.cs
@@ -5,88 +5,89 @@ using HOONS.TDD.Chapter2.TaxCalculator;
 
 namespace HOONS.TDDChapter2.TaxCalculator.Test
 {
-public class StubTaxRepository : ITaxRepository
+    public class StubTaxRepository : ITaxRepository
+    {
+        public int TaxRate { get; set; }
+
+        public int GetTaxRate(TaxYear taxYear)
+        {
+            return TaxRate;
+        }
+    }
+
+    public class StubTaxRepositoryAbstract : ATaxRepository
+    {
+        public int TaxRate { get; set; }
+
+        public override int GetTaxRate(TaxYear taxYear)
+        {
+            return TaxRate;
+        }
+
+    }
+
+    [TestFixture]
+    public class TaxCalculatorTest
+    {
+        [Test]
+        public void When2013_ShouldReturn90PercentUsingAbstractClass()
+        {
+            //Arrange
+            var taxRepo = new StubTaxRepository {TaxRate = 10};
+            var taxHelper = new TaxHelper(TaxYear.Year2013, taxRepo);
+            const int salaryExpected = 900;
+
+            //Act
+            var salaryResulted = taxHelper.Calculate(1000);
+
+            //Assert
+            Assert.That(salaryResulted, Is.EqualTo(salaryExpected));
+        }
+
+[Test]
+public void When2013_ShouldReturn90Percent()
 {
-    public int TaxRate { get; set; }
+    //Arrange
+    var taxRepo = new StubTaxRepository {TaxRate = 10};
+    var taxHelper = new TaxHelper(TaxYear.Year2013, taxRepo);
+    const int salaryExpected = 900;
 
-    public int GetTaxRate(TaxYear taxYear)
-    {
-        return TaxRate;
-    }
+    //Act
+    var salaryResulted = taxHelper.Calculate(1000);
 
-}
-public class StubTaxRepositoryAbstract : ATaxRepository
-{
-    public int TaxRate { get; set; }
-    public override int GetTaxRate(TaxYear taxYear)
-    {
-        return TaxRate;
-    }
-
+    //Assert
+    Assert.That(salaryResulted, Is.EqualTo(salaryExpected));
 }
 
-[TestFixture]
-public class TaxCalculatorTest
-{
-    [Test]
-    public void When2013_ShouldReturn90PercentUsingAbstractClass()
-    {
-        //Arrange
-        var taxRepo = new StubTaxRepository { TaxRate = 10 };
-        var taxHelper = new TaxHelper(TaxYear.Year2013, taxRepo);
-        const int salaryExpected = 900;
+        [Test]
+        public void When2014_ShouldReturn80Percent()
+        {
+            //Arrange
+            var taxRepo = new StubTaxRepository {TaxRate = 20};
+            var taxHelper = new TaxHelper(TaxYear.Year2013, taxRepo);
+            const int salaryExpected = 800;
 
-        //Act
-        var salaryResulted = taxHelper.Calculate(1000);
+            //Act
+            var salaryResulted = taxHelper.Calculate(1000);
 
-        //Assert
-        Assert.That(salaryResulted, Is.EqualTo(salaryExpected));
+            //Assert
+            Assert.That(salaryResulted, Is.EqualTo(salaryExpected));
+        }
+
+        [TestCase(TaxYear.Year2013)]
+        [TestCase(TaxYear.Year2014)]
+        public void WhenSalaryIsZero_ShouldReturnZero(TaxYear taxYear)
+        {
+            //Arrange
+            var taxRepo = new StubTaxRepository {TaxRate = 10};
+            var taxHelper = new TaxHelper(TaxYear.Year2013, taxRepo);
+            const int salaryExpected = 0;
+
+            //Act
+            var salaryResulted = taxHelper.Calculate(0);
+
+            //Assert
+            Assert.That(salaryResulted, Is.EqualTo(salaryExpected));
+        }
     }
-
-    [Test]
-    public void When2013_ShouldReturn90Percent()
-    {
-        //Arrange
-        var taxRepo = new StubTaxRepository {TaxRate = 10};
-        var taxHelper = new TaxHelper(TaxYear.Year2013, taxRepo);
-        const int salaryExpected = 900;
-
-        //Act
-        var salaryResulted = taxHelper.Calculate(1000);
-
-        //Assert
-        Assert.That(salaryResulted, Is.EqualTo(salaryExpected));
-    }
-
-    [Test]
-    public void When2014_ShouldReturn80Percent()
-    {
-        //Arrange
-        var taxRepo = new StubTaxRepository {TaxRate = 20};
-        var taxHelper = new TaxHelper(TaxYear.Year2013, taxRepo);
-        const int salaryExpected = 800;
-
-        //Act
-        var salaryResulted = taxHelper.Calculate(1000);
-
-        //Assert
-        Assert.That(salaryResulted, Is.EqualTo(salaryExpected));
-    }
-
-    [TestCase(TaxYear.Year2013)]
-    [TestCase(TaxYear.Year2014)]
-    public void WhenSalaryIsZero_ShouldReturnZero(TaxYear taxYear)
-    {
-        //Arrange
-        var taxRepo = new StubTaxRepository {TaxRate = 10};
-        var taxHelper = new TaxHelper(TaxYear.Year2013, taxRepo);
-        const int salaryExpected = 0;
-
-        //Act
-        var salaryResulted = taxHelper.Calculate(0);
-
-        //Assert
-        Assert.That(salaryResulted, Is.EqualTo(salaryExpected));
-    }
-}
 }
